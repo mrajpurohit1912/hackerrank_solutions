2686a42b0de68ba31d90c1b7907aaa02df7bfa61
mrajourohit1912 <mrajpurohit1912@gmail.com>
2023-06-03 21:19:26 +0530

    The Minion Game

---
+++ hackerrank_solution.py
@@ -0,0 +1,23 @@
+#The Minion Game
+
+def minion_game(string):
+    vowels = "AEIOU"
+    stuart_score = 0
+    kevin_score = 0
+    len_str = len(string)
+    for i in range(len_str):
+        if string[i] in vowels:
+            kevin_score += 1
+        else:
+            stuart_score += 1
+
+    if stuart_score > kevin_score:
+        print("Stuart {}".format(stuart_score))
+    elif kevin_score > stuart_score:
+        print("Kevin {}".format(kevin_score))
+    else:
+        print("Draw")
+
+if __name__ == '__main__':
+    s = input()
+    minion_game(s)
