6155d16665eda8875a1fcd899914ddf27c549513
mrajourohit1912 <mrajpurohit1912@gmail.com>
2023-06-04 19:48:20 +0530

    itertools.product() solutions

--- hackerrank_solution.py
+++ hackerrank_solution.py
@@ -1,23 +1,35 @@
 #The Minion Game
 
-def minion_game(string):
-    vowels = "AEIOU"
-    stuart_score = 0
-    kevin_score = 0
-    len_str = len(string)
-    for i in range(len_str):
-        if string[i] in vowels:
-            kevin_score += 1
-        else:
-            stuart_score += 1
+# def minion_game(string):
+#     vowels = "AEIOU"
+#     stuart_score = 0
+#     kevin_score = 0
+#     len_str = len(string)
+#     for i in range(len_str):
+#         if string[i] in vowels:
+#             kevin_score += 1
+#         else:
+#             stuart_score += 1
 
-    if stuart_score > kevin_score:
-        print("Stuart {}".format(stuart_score))
-    elif kevin_score > stuart_score:
-        print("Kevin {}".format(kevin_score))
-    else:
-        print("Draw")
+#     if stuart_score > kevin_score:
+#         print("Stuart {}".format(stuart_score))
+#     elif kevin_score > stuart_score:
+#         print("Kevin {}".format(kevin_score))
+#     else:
+#         print("Draw")
 
-if __name__ == '__main__':
-    s = input()
-    minion_game(s)
+# if __name__ == '__main__':
+#     s = input()
+#     minion_game(s)
+
+
+
+#itertools.product() 
+
+# from itertools import product
+
+# a = list(map(int,input().split()))
+# b = list(map(int,input().split()))
+
+
+# print(list(product(a,b)))
