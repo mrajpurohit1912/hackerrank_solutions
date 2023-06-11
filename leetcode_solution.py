c5ed3f3d095e2e71cab6a54695a2936508920518
mrajourohit1912 <mrajpurohit1912@gmail.com>
2023-06-11 13:24:07 +0530

    Leetcode_solutions

---
+++ leetcode_solution.py
@@ -0,0 +1,16 @@
+#1. Two Sum
+
+# class Solution(object):
+#     def twoSum(self, nums, target):
+#         sol = []
+#         for i in nums:
+#             for j in range(i+1,len(nums)):
+#                 if (nums[i] + nums[j]) == target:
+#                     sol.append(i)
+#                     sol.append(j)
+#                     break
+#                 else:
+#                     continue
+#         return sol
+    
+############################################################################################
